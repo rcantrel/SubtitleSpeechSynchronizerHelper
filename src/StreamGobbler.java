410e1464b234c2d87d4385c5a2302327d41c1463
Randall Cantrell <rcantrel@gmail.com>
2020-12-19 00:52:07 -0500

    ubuntu modifications

--- src/StreamGobbler.java
+++ src/StreamGobbler.java
@@ -5,8 +5,9 @@ import java.io.InputStreamReader;
 
 
 public class StreamGobbler extends Thread {
-    InputStream is;
-    String type;
+	private static final String BLANK_STRING = "";
+    private InputStream is;
+    private String type;
 
     public StreamGobbler(InputStream is, String type) {
         this.is = is;
@@ -19,8 +20,11 @@ public class StreamGobbler extends Thread {
             InputStreamReader isr = new InputStreamReader(is);
             BufferedReader br = new BufferedReader(isr);
             String line = null;
-            while ((line = br.readLine()) != null)
-                System.out.println(type + "> " + line);
+            while ((line = br.readLine()) != null) {
+            	if(BLANK_STRING.equals(line.trim())) {
+            		System.out.println("    " + type + "> " + line);
+            	}
+            }
         }
         catch (IOException ioe) {
             ioe.printStackTrace();
